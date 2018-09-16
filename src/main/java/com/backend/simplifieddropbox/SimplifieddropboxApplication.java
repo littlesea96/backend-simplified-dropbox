c97fde71a0cd620f16ae230b804d3fbd1b6bec80
Soraya Sakonwittayanon <soraya.sakonwittayanon@gmail.com>
2018-09-17 00:35:14 +0700

    initialize spring with web security

---
+++ src/main/java/com/backend/simplifieddropbox/SimplifieddropboxApplication.java
@@ -0,0 +1,12 @@
+package com.backend.simplifieddropbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifieddropboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifieddropboxApplication.class, args);
+	}
+}
